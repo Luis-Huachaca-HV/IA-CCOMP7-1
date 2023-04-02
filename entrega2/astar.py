aeb5449085c854a6efe99816ea34cab12d4635aa
Luis-Huachaca-HV <74331112+Luis-Huachaca-HV@users.noreply.github.com>
2023-04-01 20:23:42 -0500

    codigo astarclickeable y respetando el grid

--- entrega2/astar.py
+++ entrega2/astar.py
@@ -1,14 +1,74 @@
+from ctypes import sizeof
+from os import remove
 import networkx as nx
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import math
+import sys
+import random
+import os
 
-# Define a function to get user input for the start and end nodes
+
+def delNodes(G, porcentaje, cantidad_nodos, ini, fin):
+    porcentajeInt = math.floor((cantidad_nodos-2)*porcentaje/100)
+    print("Porcentaje",porcentajeInt)
+    eliminados=[]
+    nodo_eliminado=-1
+    for i in range(0,porcentajeInt):
+        while nodo_eliminado == ini or nodo_eliminado == fin or eliminados.count(nodo_eliminado)!=0 or nodo_eliminado==-1:
+            nodo_eliminado=random.randint(0, cantidad_nodos-1)
+        G.remove_node(nodo_eliminado)
+        eliminados.append(nodo_eliminado)
+    return len(eliminados)
+
+def initGrafo(G, num):
+    contador = 0
+    for i in range(0,num):
+        if(i == 0):
+            G.add_node(contador,pos = (0,i))
+            contador=contador+1
+        else:
+            G.add_node(contador,pos = (0,i))
+            contador=contador+1
+            G.add_edge(i,i-1, peso = 1)
+
+    for i in range(1,num+(num-1)):
+        if(i%2==1):
+            for j in range (0,num-1):
+                G.add_node(contador,pos=(i,0.5+(j*1)))
+                G.add_edge(contador,contador-num,peso=math.sqrt(2)/2)
+                G.add_edge(contador,contador-num+1,peso=math.sqrt(2)/2)
+                contador=contador+1
+        else:
+            for j in range(0,num):
+                
+                if(j==0):
+                    G.add_node(contador,pos=(i,j))
+                    G.add_edge(contador,contador-(num+(num-1)),peso=1)
+                    G.add_edge(contador,contador-num+1,peso=math.sqrt(2)/2)
+                    contador=contador+1
+                    
+                elif(j==num-1):
+                    G.add_node(contador,pos=(i,j))
+                    G.add_edge(contador,contador-(num+(num-1)),peso=1)
+                    G.add_edge(contador,contador-num,peso=math.sqrt(2)/2)
+                    G.add_edge(contador,contador-1,peso=1)
+                    contador=contador+1
+                else:
+                    G.add_node(contador,pos=(i,j))
+                    G.add_edge(contador,contador-(num+(num-1)),peso=1)
+                    G.add_edge(contador,contador-num+1,peso=math.sqrt(2)/2)
+                    G.add_edge(contador,contador-num,peso=math.sqrt(2)/2,)
+                    G.add_edge(contador,contador-1,peso=1)
+                    contador=contador+1
+    return contador
+
 def get_start_end_nodes(graph):
     start_node = None
     end_node = None
     while start_node is None or end_node is None:
         # Draw the graph with the current start and end nodes highlighted
         plt.clf() # Clear the previous graph
-        pos = nx.spring_layout(graph)
+        pos = nx.get_node_attributes(graph, 'pos')
         nx.draw_networkx(graph, pos, node_color=['yellow' if node == start_node else 'green' if node == end_node else 'white' for node in graph.nodes()])
         plt.draw()
         plt.pause(0.001)
@@ -30,7 +90,6 @@ def get_start_end_nodes(graph):
     
     return start_node, end_node
 
-# Define the A* algorithm using the networkx implementation
 def find_shortest_path(graph, start, end):
     # Set the weight attribute for all nodes in the graph
     nx.set_node_attributes(graph, 0, 'weight')
@@ -40,18 +99,15 @@ def find_shortest_path(graph, start, end):
     return path
 
 
-# Define the main function that creates the graph and runs the A* algorithm
 def main():
-    # Create the graph
     G = nx.Graph()
-    G.add_edge('A', 'B', weight=2)
-    G.add_edge('A', 'C', weight=1)
-    G.add_edge('B', 'D', weight=3)
-    G.add_edge('C', 'D', weight=1)
-    G.add_edge('C', 'E', weight=2)
-    G.add_edge('D', 'E', weight=1)
+    visitados=[]
+
+    print("Elige el tamaÃ±o del grafo: ")
+    tam = int(input())
+    numero_nodos=initGrafo(G, tam)
     
-    # Get the start and end nodes from the user
+    
     start_node, end_node = get_start_end_nodes(G)
     
     # Find the shortest path using A*
@@ -59,9 +115,9 @@ def main():
     
     # Draw the final graph with the shortest path highlighted
     plt.clf() # Clear the previous graph
-    pos = nx.spring_layout(G)
+    pos = nx.get_node_attributes(G, 'pos') # get the grid positions of each node
     nx.draw_networkx(G, pos, node_color=['yellow' if node == start_node else 'green' if node == end_node else 'white' for node in G.nodes()])
-    nx.draw_networkx_edges(G, pos, edgelist=[(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)], edge_color='r', width=5)
+    nx.draw_networkx_edges(G, pos, edgelist=[(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)], edge_color='r', width=2)
     plt.show()
 
 if __name__ == '__main__':
